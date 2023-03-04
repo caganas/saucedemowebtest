18538903b9d501fb712bcd13407092a4b4204f4b
Cagan <c.nasuhoglu@gmail.com>
2023-03-04 22:26:05 +0300

    final code

--- TestProjectSauceDemo/tests/E2ETest.cs
+++ TestProjectSauceDemo/tests/E2ETest.cs
@@ -1,6 +1,9 @@
+using AventStack.ExtentReports.Model;
+using log4net;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools;
 using OpenQA.Selenium.Support.UI;
 using System.Reflection;
 using TestProjectSauceDemo.pageObjects;
@@ -13,29 +16,37 @@ namespace TestProjectSauceDemo.tests
     {
 
 
-        [Test, TestCaseSource("AddTestDataConfig")]
+        [Test, TestCaseSource("AddTestDataConfig"), Category("Regression")]
         public void EndToEndFlow(string username, string password, string name, string surname, string postcode)
         {
-          
+            Log.Info("Username: " + username + "and Surname: " + surname + " will login");
             LoginPage loginPage = new LoginPage(getDriver());
-            ProductsPage productPage= loginPage.validLogin(username,password);
+            ProductsPage productPage = loginPage.validLogin(username, password);
             productPage.waitForProductPage();
+            Log.Info("User successfully entired to the web site.");
+            Log.Info("Products will be selected");
             productPage.selectProducts();
-            YourCardPage yourCardPage= productPage.clickShopCardLinkButton();
-            CheckoutPage checkoutPage=yourCardPage.clickCheckoutButton();
-            Thread.Sleep(4000);
+            Log.Info("Shop card link button will be clicked.");
+            YourCardPage yourCardPage = productPage.clickShopCardLinkButton();
+            Log.Info("Checkout button will be clicked.");
+            CheckoutPage checkoutPage = yourCardPage.clickCheckoutButton();
+            Log.Info("User Adress information will be written.");
             checkoutPage.enterYourInformation(name, surname, postcode);
-            OverviewPage overviewPage=checkoutPage.clickContinueButton();
+            Log.Info("Contyinue button will be clicked");
+            OverviewPage overviewPage = checkoutPage.clickContinueButton();
+            Log.Info("Overview page will be opened.");
             overviewPage.waitforOverviewPage();
+            Log.Info("Finish button will be clicked.");
             overviewPage.clickFinishButton();
+            Log.Info("Order Completed message will be taken.");
             overviewPage.checkOrderCompletedMessage();
-
+            Log.Info("Test is successfull.");
 
         }
 
         public static IEnumerable<TestCaseData> AddTestDataConfig()
         {
-            yield return new TestCaseData(getDataParser().extractData("username") , getDataParser().extractData("password"), getDataParser().extractData("name"), getDataParser().extractData("surname"), getDataParser().extractData("postcode"));
+            yield return new TestCaseData(getDataParser().extractData("username"), getDataParser().extractData("password"), getDataParser().extractData("name"), getDataParser().extractData("surname"), getDataParser().extractData("postcode"));
             yield return new TestCaseData(getDataParser().extractData("performanceglitchuser"), getDataParser().extractData("password"), getDataParser().extractData("name"), getDataParser().extractData("surname"), getDataParser().extractData("postcode"));
 
         }
--- TestProjectSauceDemo/utilities/Base.cs
+++ TestProjectSauceDemo/utilities/Base.cs
@@ -10,27 +10,48 @@ using WebDriverManager.DriverConfigs.Impl;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Edge;
 using System.Configuration;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports;
+using ICSharpCode.SharpZipLib.Zip;
+using System.Data;
+using log4net;
 
 namespace TestProjectSauceDemo.utilities
 {
     public class Base
     {
-
+        public ILog Log;
+        public ExtentReports extent;
+        public ExtentTest test;
+        String browserName;
         public IWebDriver driver;
+
+        //reporting
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            Log = LogManager.GetLogger(GetType());
+
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            string reportPath = projectDirectory + "//index.html";
+            var htmlReporter= new ExtentHtmlReporter(reportPath);
+            extent = new ExtentReports();
+            extent.AttachReporter(htmlReporter);
+            extent.AddSystemInfo("Host Name", "Local Host");
+            extent.AddSystemInfo("Username", "Galip Cagan Nasuhoglu");
+        }
 
         [SetUp]
         public void StartBrowser()
         {
+            test = extent.CreateTest(TestContext.CurrentContext.Test.Name);
             //Configuration
-            String browserName = ConfigurationManager.AppSettings["browser"];
+            browserName = ConfigurationManager.AppSettings["browser"];
             InitBrowser(browserName);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
             driver.Manage().Window.Maximize();
-            driver.Url = "https://www.saucedemo.com/";
-            var chromeOptions = new ChromeOptions();
-            chromeOptions.AddArgument("-no-sandbox");
-            //Driver.Navigate().GoToUrl(ConfigurationManager.AppSettings["URL"]);
-            //driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            driver.Navigate().GoToUrl(ConfigurationManager.AppSettings["URL"]);
         }
 
 
@@ -54,6 +75,8 @@ namespace TestProjectSauceDemo.utilities
 
                     new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
                     driver = new ChromeDriver();
+                    var chromeOptions = new ChromeOptions();
+                    chromeOptions.AddArgument("-no-sandbox");
                     break;
 
 
@@ -70,7 +93,29 @@ namespace TestProjectSauceDemo.utilities
         [TearDown]
         public void AfterTest()
         {
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+            var stackTrace = TestContext.CurrentContext.Result.StackTrace;
+            DateTime time = DateTime.Now;
+            String fileName = "Screenshot"+ time.ToString("h_mm_ss")+".png";
+            if(status == NUnit.Framework.Interfaces.TestStatus.Failed)
+            {
+                test.Fail("Test failed", captureScreenhot(driver,fileName));
+                test.Log(Status.Fail, "test failed with logtrace" + stackTrace);
+            }
+            else if(status==NUnit.Framework.Interfaces.TestStatus.Passed)
+            {
+
+            }
+
+            extent.Flush();
             driver.Quit();
+        }
+
+        public MediaEntityModelProvider captureScreenhot (IWebDriver driver, String screenShotName)
+        {
+            ITakesScreenshot ts=(ITakesScreenshot)driver;
+            var screenshot= ts.GetScreenshot().AsBase64EncodedString;
+           return MediaEntityBuilder.CreateScreenCaptureFromBase64String(screenshot, screenShotName).Build();
         }
 
 
